ca098013869212a79b8bf480a20dbbfab3920fbe
hproux <hugo.proux@live.fr>
2020-03-16 10:35:31 +0100

    ajout composer

--- app/conf/settings.php
+++ app/conf/settings.php
@@ -5,18 +5,5 @@ return [
     'settings' => [
         'displayErrorDetails' => true,
         'dbconf' => '../conf/config.ini',
-        'validatorAddCommand' => [
-            'nom' => Validator::alpha("é à è ù ô î â ï ë ö"),
-            'mail' =>  Validator::email(),
-            'livraison' => [
-                'date' => Validator::date('d-m-Y')->min('now'),
-                'heure' => Validator::date('H:i:s')
-            ],
-            'client_id' => Validator::optional(Validator::intVal()),
-            'items' => [
-                //'uri' => Validator::alnum('/'),
-                //'q' => Validator::numeric()
-            ]
-        ]
     ]
 ];
