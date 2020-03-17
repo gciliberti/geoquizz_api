ec1bf8658b4d86c087ae50babe4429e75c6a0af6
gciliberti <g.ciliberti@protonmail.com>
2020-03-17 12:23:03 +0100

    cors issue

--- player/api/index.php
+++ player/api/index.php
@@ -19,7 +19,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://localhost')
+        ->withHeader('Access-Control-Allow-Origin', 'http://134.122.67.129/player/api/index.php')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
