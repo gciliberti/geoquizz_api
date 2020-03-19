9f27c501099c362c52db002fe1b1d9d1486b4771
gciliberti <g.ciliberti@protonmail.com>
2020-03-19 15:33:15 +0100

    cors issue

--- backoffice/api/index.php
+++ backoffice/api/index.php
@@ -31,18 +31,16 @@ $updateSerieValidator = [
 
 DatabaseConnection::startEloquent(($app->getContainer())->settings['dbconf']);
 
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
-});
-
-
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Origin', 'https://api.tallium.tech/backoffice/api/index.php')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Credentials', 'true')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
 });
+
 
 $app->add(new Tuupola\Middleware\JwtAuthentication([
     "ignore" => ["/login", "/register"],
