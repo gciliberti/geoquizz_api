46efb92f13c6f3f2fce9f8847ec357b198b79fdf
gciliberti <g.ciliberti@protonmail.com>
2020-03-19 15:35:22 +0100

    fix cors

--- backoffice/api/index.php
+++ backoffice/api/index.php
@@ -34,7 +34,7 @@ DatabaseConnection::startEloquent(($app->getContainer())->settings['dbconf']);
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', 'https://api.tallium.tech/backoffice/api/index.php')
+        ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Credentials', 'true')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
