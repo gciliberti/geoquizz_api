e56732be2e242f829fba94abf65280f7dd0b8902
hproux <hugo.proux@live.fr>
2020-03-24 14:02:25 +0100

    ajout middleware

---
+++ app/src/middleware/Validator.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace geoquizz\app\middleware;
+
+use geoquizz\app\utils\Writer;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class Validator
+{
+    public static function dataFormatErrorHandler(Request $request, Response $response, callable $next) {
+        if ($request->getAttribute('has_errors')) {
+            return Writer::jsonResponse($response, 400, [
+                "type" => "error",
+                "error" => 400,
+                "message" => "Parametres mal formes.",
+                "details" => $request->getAttribute('errors'),
+            ]);
+        }
+
+        return $next($request, $response);
+    }
+}
